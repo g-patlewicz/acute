5a9df3e84a11db76bd0b85b0d5c070074f887310
patlewig <patlewig@users.noreply.github.com>
2020-04-11 15:38:16 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Analysis evaluating the performance of selected expert systems for the prediction of acute oral toxicity in rodents',
+    author='patlewig',
+    license='',
+)
